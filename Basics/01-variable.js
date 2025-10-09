e1be93e4e165d9aaaba3e038b63bd789d51d81a2
Sarvesh <sarveshkhawale17@gmail.com>
2025-10-10 01:30:31 +0530

    Add files via upload

---
+++ Basics/01-variable.js
@@ -0,0 +1,11 @@
+const accountId = 226851
+let accountEmail = "SARVESH@GMAIL.COM"
+var accountpassword = "12345"
+accountcity = "Nagpur"
+
+accountEmail = "sanskruti@gmail.com "
+accountpassword = "54321"
+accountcity = "Pune"
+console.log(accountId)
+console.table([accountEmail, accountpassword, accountcity]
+)
---
+++ Basics/02-datatypes.js
@@ -0,0 +1,8 @@
+// alert(hey) we are using node.js, not browser
+
+  let name = "sarvesh" // string
+    let age = 21 // number
+    let isApproved = false // boolean
+ console.log(typeof name)
+ console.log(typeof age)
+ console.log(typeof isApproved)
