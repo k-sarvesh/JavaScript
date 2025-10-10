fdf8687d2fdc79cdfeddae92f87b830d99bc0151
Sarvesh <sarveshkhawale17@gmail.com>
2025-10-10 22:57:29 +0530

    Delete Basics directory

--- Basics/02-datatypes.js
+++
@@ -1,8 +0,0 @@
-// alert(hey) we are using node.js, not browser
-
-  let name = "sarvesh" // string
-    let age = 21 // number
-    let isApproved = false // boolean
- console.log(typeof name)
- console.log(typeof age)
- console.log(typeof isApproved)
